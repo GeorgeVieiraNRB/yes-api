d39dac3d3c6f5827218056908969edcd54005c8f
GeorgeVieiraNRB <georgevieiranrb@gmail.com>
2026-06-21 22:34:29 -0300

    refactor: changing struture of server setup files

---
+++ src/server.ts
@@ -0,0 +1,6 @@
+import { app } from "./app";
+import { environment } from "./config/environment";
+
+app.listen(environment.PORT, () => {
+  console.log(`Server running on http://localhost:${environment.PORT}`);
+});
